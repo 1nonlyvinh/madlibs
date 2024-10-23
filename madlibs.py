fd96e978805fed59fe08f95c4f1cf4eca8db1aa9
1nonlyvinh <vincenth2898@tuckahoeschools.org>
2024-10-23 12:26:30 +0000

    brainrot madlib

---
+++ madlibs.py
@@ -0,0 +1,22 @@
+adjective1 = input("Type an Adjective:")
+friendadj = input ("Type an Adjective for his friend:")
+noun1 = input("Type a noun:")
+noun2 = input("Type another noun:")
+name = input("What's his name:")
+dreamthing1 = input("Type what he saw in his dream:")
+dreamthing2 = input("Type what he saw in his dream:")
+
+
+print(f"""Once upon a time there was a {adjective1} {noun1} named {name}. {noun1} walked to school where he was greeted by his friend, {noun2},
+he started talking about how he had a dream about it was about {dreamthing1} and {dreamthing2}. He said all he saw were {dreamthing2} 
+and {dreamthing1} and it was very weird. """) 
+
+brainrot1 = input("Type brainrot:")
+brainrot2 = input("Type brainrot:") 
+friendadj2 = input("Type an adjective for their friends")
+weirdstuff1 = input("Type a weird thing")
+weirdstuff2 = input("Type a weird thing")
+
+print(f"""{name} went to his first class where all his classmates, were spewing brainrot. "{brainrot1}" they said, and "{brainrot2}". {name} immedietly left the school
+and went back home. His mom wondered why {name} came back home. To which he promptly replied, "all my friends are {friendadj2} they kept talking
+about weird stuff like {weirdstuff1} and {weirdstuff2}. They are just {friendadj2}, that's why I came back home."""")
